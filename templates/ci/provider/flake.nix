04af0f4fd97d943990eb59c87b38954a4c91d795
Jason Bowman <jason@json64.dev>
2026-05-21 15:37:40 -0700

    chore: rename den-schema to gen-schema
    Update flake inputs and references to match the renamed repo
    at github:sini/gen-schema.

--- templates/ci/provider/flake.nix
+++ templates/ci/provider/flake.nix
@@ -10,7 +10,7 @@
     nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
     import-tree.url = "github:vic/import-tree";
     den.url = "github:denful/den";
-    den-schema.url = "github:sini/den-schema";
-    den-schema.inputs.nixpkgs.follows = "nixpkgs";
+    gen-schema.url = "github:sini/gen-schema";
+    gen-schema.inputs.nixpkgs.follows = "nixpkgs";
   };
 }
